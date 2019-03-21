239aae37d4e1457836b0dfce30efbd9d14d6b50f
Mayara <mayaradiasg@gmail.com>
2019-03-21 17:15:26 -0300

    added Procfile

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -78,11 +78,6 @@ WSGI_APPLICATION = 'warehouse.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
-
     'default':{
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'd4sgfm7nln1cl7',
