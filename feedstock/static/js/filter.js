ad345a82424c1ca67c2e4ee6e42708a0e00f7d44
Mayara <mayaradiasg@gmail.com>
2019-03-22 11:47:23 -0300

    added filter by id on feedstock_list template

--- feedstock/static/js/filter.js
+++ feedstock/static/js/filter.js
@@ -3,15 +3,17 @@ function myFunction() {
   var input, filter, table, tr, td, i, txtValue, tbody;
   input = document.getElementById("search");
   filter = input.value.toUpperCase();
-  table = document.getElementById("feedstock-table");;
+  table = document.getElementById("feedstock-table");
   tr = table.getElementsByTagName("tr");
 
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[1];
-    if (td) {
+    id = tr[i].getElementsByTagName("td")[0];
+    if (td && id) {
       txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      idValue = id.textContent || id.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1 || idValue.indexOf(filter) > -1) {
         tr[i].style.display = "";
       } else {
         tr[i].style.display = "none";
